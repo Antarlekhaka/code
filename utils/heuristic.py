900078ee3c8d8420865f74f875e108b8bf5c0e8d
Hrishikesh Terdalkar <hrishirt@cse.iitk.ac.in>
2022-08-04 09:06:47 +0530

    feat: anvaya heuristic

--- utils/heuristic.py
+++ utils/heuristic.py
@@ -14,11 +14,44 @@ from typing import Dict, List
 
 ###############################################################################
 
-ANVAYA_CASE_ORDER = ['Nom', 'Loc', 'Dat', 'Abl', 'Ins', 'Acc']
-ANVAYA_XPOS_ORDER = ['NC', r'.*', 'V']
+ANVAYA_CASE_ORDER = ["Loc", "Nom", "Dat", "Abl", "Ins", "Acc"]
+ANVAYA_XPOS_ORDER = ["CAD", "CX", "CNG", "V"]
 
 ###############################################################################
 
 
-def anvaya(token_list: List[Dict]) -> List[int]:
-    return list(token_list)
+def get_anvaya(token_list: Dict[int, Dict]) -> List[int]:
+    print(token_list)
+    case_order = []
+    xpos_order = []
+    used = set()
+    unused = set(token_list)
+    for case in ANVAYA_CASE_ORDER:
+        for token_id, token in token_list.items():
+            if token["analysis"].get("feats", {}).get("Case") == case:
+                case_order.append(token_id)
+                unused.remove(token_id)
+                used.add(token_id)
+
+    for xpos in ANVAYA_XPOS_ORDER:
+        for token_id, token in token_list.items():
+            if token_id in used:
+                continue
+            if token["analysis"].get("xpos") == xpos:
+                xpos_order.append(token_id)
+                unused.remove(token_id)
+                used.add(token_id)
+
+    anvaya_order = []
+    anvaya_order.extend(case_order)
+
+    for token_id, token in token_list.items():
+        if token_id in unused:
+            anvaya_order.append(token_id)
+            unused.remove(token_id)
+            used.add(token_id)
+
+    anvaya_order.extend(xpos_order)
+    assert set(token_list) == set(anvaya_order)
+
+    return case_order
